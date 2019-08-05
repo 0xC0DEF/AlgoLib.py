304c08743de769e63f694ab0f4fc22253e824430
0xC0DEF <lobo_prix@naver.com>
2019-08-05 21:24:42 +0900

    Update fft.py

--- fft.py
+++ fft.py
@@ -3,18 +3,23 @@ import math
 def topow2(x):
   return 1 if x==0 else 1<<(x-1).bit_length()
 
-def fft(a):
+def fftr(a,w):
   n=len(a)
   if n==1:
     return a
   even=[];odd=[]
   for i,x in enumerate(a):
     (even if i%2==0 else odd).append(x)
-  even=fft(even);odd=fft(odd)
+  even=fftr(even,w*w);odd=fftr(odd,w*w)
   ret=[]
+  ww=1
   for i in range(n):
-    ret.append(even[i%(n//2)]+math.e**(-2*math.pi*complex(0,1)*i/n)*odd[i%(n//2)])
+    ret.append(even[i%(n//2)]+ww*odd[i%(n//2)])
+    ww*=w
   return ret
+
+def fft(a):
+  return fftr(a,math.e**(-2*math.pi*complex(0,1)/len(a)))
 
 def ffti(a):
   a[1:]=reversed(a[1:])
@@ -30,7 +35,7 @@ def conv_mul(a,b):
   n=len(a)
   a.extend([0]*(topow2(n*2)-n))
   b.extend([0]*(topow2(n*2)-n))
-  return conv(a,b)[:n]
+  return conv(a,b)
 
 def conv_cir(a,b):
   a=list(a);b=list(b)
