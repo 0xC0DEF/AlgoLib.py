9a55519e24af268443ae480c60495f95cdb6e62b
0xC0DEF <lobo_prix@naver.com>
2020-03-04 18:13:35 +0900

    Update fft_bigint.py

--- fft_bigint.py
+++ fft_bigint.py
@@ -1,16 +1,13 @@
 def conv_mul(a,b):
-	a=int(''.join(list(map(lambda x:('{:0'+str(M)+'b}').format(x), a))),2)
-	b=int(''.join(list(map(lambda x:('{:0'+str(M)+'b}').format(x), b))),2)
+	a=int(''.join(list(map(lambda x:('{:0'+str(M)+'b}').format(x), a[::-1]))),2)
+	b=int(''.join(list(map(lambda x:('{:0'+str(M)+'b}').format(x), b[::-1]))),2)
 	c=format(a*b,'b')[::-1]
 	d=[]
 	for i in range(0,len(c),M):
 		d.append(int(c[i:min(i+M,len(c))][::-1],2))
 	return d
 
-M=30
-n=int(input())
-a=list(map(int,input().split()))[::-1]
-b=list(map(int,input().split()))
-a+=a
-b+=[0]*len(b)
-print(max(conv_mul(a,b)))
+def conv_cyc(a,b):
+	a+=a
+	b+=[0]*len(b)
+	return conv_mul(a,b)
